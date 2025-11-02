26e35b0ea6c6bae6b21a0750d1a570566da23f68
joselimahuaya27 <joselimahuaya27@gmail.com>
2025-11-01 19:56:39 -0500

    pruebapro

---
+++ NetBeansProjects/proyectoprueba/src/main/java/com/mycompany/proyectoprueba/Proyectoprueba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoprueba;
+
+/**
+ *
+ * @author W11
+ */
+public class Proyectoprueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
