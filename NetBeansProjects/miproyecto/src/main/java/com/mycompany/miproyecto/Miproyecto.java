2f14ead81095649e1a344504a4e5fdeb10298c2a
joselimahuaya27 <joselimahuaya27@gmail.com>
2025-11-13 23:51:36 -0500

    carga inicial

---
+++ NetBeansProjects/miproyecto/src/main/java/com/mycompany/miproyecto/Miproyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.miproyecto;
+
+/**
+ *
+ * @author W11
+ */
+public class Miproyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
