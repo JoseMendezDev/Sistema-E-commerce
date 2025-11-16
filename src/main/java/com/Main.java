fa21df375fd0a8408baaee7fd94cc8dc97b52878
joselimahuaya27 <joselimahuaya27@gmail.com>
2025-11-16 07:08:23 -0500

    inicializacion de scanner main

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package main.java.com;
 
+package main.java.com;
+import java.util.*;
 /**
  *
  * @author USER
  */
 public class Main {
+ private static Scanner scanner = new Scanner(System.in);
     
 }
