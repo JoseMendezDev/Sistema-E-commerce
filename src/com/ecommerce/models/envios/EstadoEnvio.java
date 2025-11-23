b10d962ce8caa47a19d817f7affcffc3595f7d8a
JoseMendezDev <josemendez.jlm.jm@gmail.com>
2025-11-23 08:24:13 -0500

    Se añade clase enum EstadoEnvio.

---
+++ src/com/ecommerce/models/envios/EstadoEnvio.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ecommerce.models.envios;
+
+/**
+ *
+ * @author USER
+ */
+public enum EstadoEnvio {
+    PREPARANDO, EN_TRANSITO, EN_REPARTO, ENTREGADO, DEVUELTO
+}
